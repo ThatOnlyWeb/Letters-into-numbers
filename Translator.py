993dd77557a14803f0bfca76d00ea9f35d76b572
ThatOnlyWeb <95704278+ThatOnlyWeb@users.noreply.github.com>
2022-01-15 09:19:52 +0000

    Create Translator.py

---
+++ Translator.py
@@ -0,0 +1,80 @@
+letter1 = 'a'
+letter2 = 'b'
+letter3 = 'c'
+letter4 = 'd'
+letter5 = 'e'
+letter6 = 'f'
+letter7 = 'g'
+letter8 = 'h'
+letter9 = 'i'
+letter10 = 'j'
+letter11 = 'k'
+letter12 = 'l'
+letter13 = 'm'
+letter14 = 'n'
+letter15 = 'o'
+letter16 = 'p'
+letter17 = 'q'
+letter18 = 'r'
+letter19 = 's'
+letter20 = 't'
+letter21 = 'u'
+letter22 = 'v'
+letter23 = 'w'
+letter24 = 'x'
+letter25 = 'y'
+letter26 = 'z'
+print("please input the amount of letters in the word you want to print, input the word letters in numbers", "max = 26")
+input = input()
+if input == "1":
+    print(letter1)
+if input == "2":
+    print(letter2)
+if input == "3":
+    print(letter3)
+if input == "4":
+    print(letter4)
+if input == "5":
+    print(letter5)
+if input == "6":
+    print(letter6)
+if input == "7":
+    print(letter7)
+if input == "8":
+    print(letter8)
+if input == "9":
+    print(letter9)
+if input == "10":
+    print(letter10)
+if input == "11":
+    print(letter11)
+if input == "12":
+    print(letter12)
+if input == "13":
+    print(letter13)
+if input == "14":
+    print(letter14)
+if input == "15":
+    print(letter15)
+if input == "16":
+    print(letter16)
+if input == "17":
+    print(letter17)
+if input == "18":
+    print(letter18)
+if input == "19":
+    print(letter19)
+if input == "20":
+    print(letter20)
+if input == "21":
+    print(letter21)
+if input == "22":
+    print(letter22)
+if input == "23":
+    print(letter23)
+if input == "24":
+    print(letter24)
+if input == "25":
+    print(letter25)
+if input == "26":
+    print(letter26)
